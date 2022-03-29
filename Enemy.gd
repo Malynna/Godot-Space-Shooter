773c76d7a16b887cb6117fd02ec68aa5b5949c7a
Malynna <malinex@op.pl>
2022-03-29 20:31:53 +0200

    Added EnemySpowner

--- Enemy.gd
+++ Enemy.gd
@@ -6,3 +6,14 @@ export(int) var ARMOR = 3
 
 func _process(delta):
 	position.x -= SPEED * delta
+
+
+func _on_Enemy_body_entered(body):
+	body.queue_free()
+	ARMOR -=1
+	if ARMOR == 0: #or <=
+		queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- Ship.gd
+++ Ship.gd
@@ -18,3 +18,8 @@ func fire_bullet():
 	main.add_child(bullet)
 	bullet.global_position = global_position
 	
+
+
+func _on_Ship_area_entered(area):
+	area.queue_free()
+	queue_free()
