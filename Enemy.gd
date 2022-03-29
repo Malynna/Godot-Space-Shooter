073e533eea440c911c4387e1e69eddef6a277b1d
Malynna <malinex@op.pl>
2022-03-29 21:22:51 +0200

    Added few code - with one bug to fix

--- Enemy.gd
+++ Enemy.gd
@@ -1,19 +1,31 @@
 extends Area2D
 
+const ExplosionEffect = preload("res://ExplsionEffect.tscn")
+
 export(int) var SPEED = 20
 export(int) var ARMOR = 3
 
+func _ready():
+	var main = get_tree().current_scene
+	if main.is_in_group("World"):
+		connect("score_up", main, "_on_Enemy_score_up")
 
 func _process(delta):
 	position.x -= SPEED * delta
 
-
 func _on_Enemy_body_entered(body):
 	body.queue_free()
 	ARMOR -=1
-	if ARMOR == 0: #or <=
+	if ARMOR <= 0: #or ==
+		emit_signal("score_up")
 		queue_free()
-
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+	
+func _exit_tree():
+	var main = get_tree().current_scene
+	var explosionEffect = ExplosionEffect.instance()
+	main.add_child(explosionEffect)
+	explosionEffect.global_position = global_position
+	
--- Ship.gd
+++ Ship.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+const ExplosionEffect = preload("res://ExplsionEffect.tscn")
 const Bullet = preload("res://Bullet.tscn")
 
 export(int) var SPEED = 100
@@ -18,7 +19,11 @@ func fire_bullet():
 	main.add_child(bullet)
 	bullet.global_position = global_position
 	
-
+func _exit_tree():
+	var main = get_tree().current_scene
+	var explosionEffect = ExplosionEffect.instance()
+	main.add_child(explosionEffect)
+	explosionEffect.global_position = global_position
 
 func _on_Ship_area_entered(area):
 	area.queue_free()
